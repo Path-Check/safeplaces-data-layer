a731dbc864d0fb4ac77285a92608abfcde2c62c0
Jeffrey Hunter <sublet@me.com>
2020-06-19 17:33:30 -0400

    Disabling access to private db on INGEST service name

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
+let privateService = {
+  accessCodeService: require('./db/private/models/accessCodes'),
+  caseService: require('./db/private/models/cases'),
+  organizationService: require('./db/private/models/organizations'),
+  pointService: require('./db/private/models/points'),
+  publicationService: require('./db/private/models/publications'),
+  publicOrganizationService: require('./db/private/models/publicOrganizations'),
+  settingService: require('./db/private/models/settings'),
+  uploadService: require('./db/private/models/upload'),
+  userService: require('./db/private/models/users')
+}
+
+let publicService = {}
+
+if (process.env.SERVICE_NAME === 'INGEST') privateService = null
+
 module.exports = {
-  private: {
-    accessCodeService: require('./db/private/models/accessCodes'),
-    caseService: require('./db/private/models/cases'),
-    organizationService: require('./db/private/models/organizations'),
-    pointService: require('./db/private/models/points'),
-    publicationService: require('./db/private/models/publications'),
-    publicOrganizationService: require('./db/private/models/publicOrganizations'),
-    settingService: require('./db/private/models/settings'),
-    uploadService: require('./db/private/models/upload'),
-    userService: require('./db/private/models/users')
-  },
-  public: {}
+  private: privateService,
+  public: publicService
 };
