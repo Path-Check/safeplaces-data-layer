caf669b45aabf39d669470279898213d6d93848f
Aiyan Jiang <mr.aiyanj@gmail.com>
2020-06-26 15:53:00 -0400

    Fix migrations for Auth0 users

---
+++ src/db/private/migrations/20200626153332_add_auth0_id.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex) {
+  return knex.schema.table('users', table => {
+    table.string('auth0_id');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.table('users', table => {
+    table.dropColumn('auth0_id');
+  });
+};
