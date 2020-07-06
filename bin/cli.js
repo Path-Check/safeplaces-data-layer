9452a34784371395b9c35c06c6142499e2b9a862
Daniel McCoy <dan.mccoy@rocketinsights.com>
2020-07-06 12:56:19 -0400

    revert version bump and cli-fix

--- bin/cli.js
+++ bin/cli.js
@@ -10,7 +10,7 @@ function success(text) {
 }
 
 function getConfig(scope, env) {
-  const configPath = path.join(__dirname, `../src/config/${scope}`);
+  const configPath = path.join(__dirname, `../config/${scope}`);
   return require(configPath)(env);
 }
 
