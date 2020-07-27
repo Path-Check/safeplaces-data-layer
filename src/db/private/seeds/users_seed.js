0c75672e0dbae93fe9e79025ee87554d14dda317
Aiyan Jiang <mr.aiyanj@gmail.com>
2020-07-27 14:21:49 -0400

    Fix user seeds for Auth0 tenant migration

---
+++ src/db/private/seeds/users_seed.js
@@ -0,0 +1,26 @@
+module.exports = function (knex) {
+  return knex('users')
+    .del() // Deletes ALL existing entries
+    .then(async function () {
+      await knex('users').insert({
+        id: 'c317216f-70b0-4538-a03e-88388beca810',
+        idm_id: 'auth0|5f1f0f23d93c10003dcb07cf',
+        organization_id: 1,
+        username: 'superuser@extremesolution.com',
+      });
+
+      await knex('users').insert({
+        id: '14718564-f521-4079-8e1b-23694290fc09',
+        idm_id: 'auth0|5f1f0f0fe6a2910037277d2d',
+        organization_id: 1,
+        username: 'safeplaces@extremesolution.com',
+      });
+
+      await knex('users').insert({
+        id: '235ed8c0-96bf-4061-b3d4-8a90b2f59585',
+        idm_id: 'auth0|5f1f0f32314999003d05021e',
+        organization_id: 1,
+        username: 'tracer@extremesolution.com',
+      });
+    });
+};
