e2cbe6a408b7a5d33f1286798fc847cab8ed3005
Daniel McCoy <dan.mccoy@rocketinsights.com>
2020-08-12 10:56:26 -0400

    remove duration column on points table

---
+++ src/db/private/migrations/20200812104804_remove_duration_column_from_points.js
@@ -0,0 +1,11 @@
+exports.up = function (knex) {
+  return knex.schema.table('points', table => {
+    table.dropColumn('duration');
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.table('points', table => {
+    table.integer('duration').default(5);
+  });
+};
